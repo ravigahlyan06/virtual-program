7607572f9655f16b9840967730606873952a755b
Ravi <ravi.gahlyan06@mail.com>
2024-03-08 17:07:58 +0530

    this is mine

---
+++ prt.js
@@ -0,0 +1,21 @@
+const menuBar = document.getElementById("bar_menu");
+const crosBtn = document.getElementById("cross_btn");
+const barBtn = document.getElementById("fa_bars");
+
+function handleShowBtn(arg) {
+  if (arg == "show") {
+    menuBar.style.display = "block";
+  } else {
+    menuBar.style.display = "none";
+  }
+}
+
+try {
+  console.log(ravi)
+} catch(err) {
+  console.log("Error", err)
+}
+
+// function handleHideBtn(){
+// }
+
